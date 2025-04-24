f70b2edfc5f16801c941cf9fd8f7da564066f7f5
Ajay Raj <ajaygupta9210p@gmail.com>
2025-04-24 23:57:54 +0530

    t

--- employees/vite.config.js
+++ employees/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
   assetsInclude: ["**/*.svg"],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
