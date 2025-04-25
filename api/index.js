a9c57dce2d3a619b9dc25bc2c91a15f0280fcc74
Ajay Raj <ajaygupta9210p@gmail.com>
2025-04-25 20:08:00 +0530

    d

--- api/index.js
+++ api/index.js
@@ -63,7 +63,13 @@ app.use((req, res, next) => {
 });
 
 // Configure CORS before other middleware (This duplicate line was removed)
-
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://0.0.0.0:3000', 'https://ekaant.onrender.com'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  exposedHeaders: ['Content-Length']
+}));
 
 // Global error handler
 app.use((err, req, res, next) => {
