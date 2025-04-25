414c02809d8a01961cb7ef2f318ca96657c41060
Ajay Raj <ajaygupta9210p@gmail.com>
2025-04-25 23:03:55 +0530

    dv

--- api/models/ProgramProgress.model.js
+++ api/models/ProgramProgress.model.js
@@ -1,42 +1,4 @@
 
-// import mongoose from 'mongoose';
-
-// const programProgressSchema = new mongoose.Schema({
-//   employeeId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Employee',
-//     required: true
-//   },
-//   programId: {
-//     type: Number,
-//     required: true
-//   },
-//   programName: {
-//     type: String,
-//     required: true
-//   },
-//   currentLevel: {
-//     type: Number,
-//     default: 0
-//   },
-//   completedLevels: [{
-//     levelIndex: Number,
-//     completedAt: Date,
-//     name: String
-//   }],
-//   startedAt: {
-//     type: Date,
-//     default: Date.now
-//   },
-//   lastAccessedAt: {
-//     type: Date,
-//     default: Date.now
-//   }
-// }, { timestamps: true });
-
-// const ProgramProgress = mongoose.model('ProgramProgress', programProgressSchema);
-// export default ProgramProgress;
-
 import mongoose from 'mongoose';
 
 const programProgressSchema = new mongoose.Schema({
@@ -45,19 +7,11 @@ const programProgressSchema = new mongoose.Schema({
     ref: 'Employee',
     required: true
   },
-  employeeEmail: {
-    type: String,
-    required: true
-  },
   programId: {
     type: Number,
     required: true
   },
   programName: {
-    type: String,
-    required: true
-  },
-  expertName: {
     type: String,
     required: true
   },
